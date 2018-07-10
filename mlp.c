f7f6183088c1b9b01ef56f40b076943d24361978
D.siva reddy <40803761+sleeva321@users.noreply.github.com>
2018-07-10 11:42:27 +0530

    Create mlp.c

---
+++ mlp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+                int x,i,mul;
+ 
+ 
+                scanf("%d",&x);
+                for(i=1;i<=5;i++)
+                {
+                    mul=x*i;
+                               printf(" %d",mul);
+                 }
+ 
+}
